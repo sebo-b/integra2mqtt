702e04e3e85cd457d580adb53f2437bdcc1c0b8b
Sebastian Baberowski <sebastian@baberowski.com>
2025-12-29 16:31:02 +0100

    v2.0

--- integra2mqtt.js
+++ integra2mqtt.js
@@ -1,9 +1,11 @@
 
-const integraIP='XX.XX.XX.XX';
-const integraPort=7094;
-const maxZones = 64;
+import secrets from "./secrets.js"
 
-const mqttURL="mqtt://USER:PASSS@HOST:PORT";
+const integraIP = secrets.integraIP;
+const integraPort=7094;
+const maxZones = 54;
+
+const mqttURL = secrets.mqttURL;
 const mqttTopic="integra";
 
 const hassDiscoveryTopic = "homeassistant";
@@ -14,7 +16,7 @@ const hassDiscoveryTopic = "homeassistant";
 import { strict as assert } from 'assert';
 import { Socket } from "net";
 import mqtt from "mqtt";
-import { setInterval as setIntervalAsync } from 'node:timers/promises';
+import { setInterval as setIntervalAsync, setTimeout as setTimeoutAsync } from 'node:timers/promises';
 
 import * as IntegraMessages from "satel-integra-integration-protocol/messages.js";
 import IntegraDecoder from "satel-integra-integration-protocol/decoder.js";
@@ -22,11 +24,7 @@ const Integra = {
     ...IntegraMessages,
     Decoder: IntegraDecoder
 };
-
 /////////// end of imports
-
-let readZoneNames = 0;
-
 function logHex(data,header='') {
 
     const lineLen = 16;
@@ -63,6 +61,17 @@ function logHex(data,header='') {
     p();
 }
 
+const Messages = Object.freeze({
+    RefreshNames: 1,
+    RunHADiscovery: 2,
+    ReadZoneStates: 3,
+});
+const messages = [
+    Messages.RefreshNames,
+    Messages.RunHADiscovery
+];
+let newMessageArrived = () => {};
+
 /////////// MQTT
 
 const commandTopic = `${mqttTopic}/cmd`;
@@ -73,9 +82,18 @@ mqttClient.subscribe(commandTopic);
 
 mqttClient.on("message", (topic, message) => {
     if (topic == commandTopic) {
-        switch (message.toString()) {
-            case "refreshZoneNames":
-                readZoneNames = 0;
+        switch (message.toString().toLowerCase()) {
+            case "refresh_names":
+                messages.push(Messages.RefreshNames);
+                newMessageArrived();
+                break;
+            case "run_ha_discovery":
+                messages.push(Messages.RunHADiscovery);
+                newMessageArrived();
+                break;
+            case "read_zone_states":
+                messages.push(Messages.ReadZoneStates);
+                newMessageArrived();
                 break;
         }
     }
@@ -83,21 +101,123 @@ mqttClient.on("message", (topic, message) => {
 
 /////////// SATEL
 
-let sock = new Socket();
-let zones = [...Array(maxZones)].map( () => ({name:'',state:undefined}));
-let fullFrameReceived = () => {};
+class IntegraCommand {
 
-async function homeAssistantDiscovery() {
+    #integraIP
+    #integraPort
+
+    async #connect(fullFramePF) {
+
+        let sock = new Socket();
+
+        return new Promise( (res,rej) => {
+
+            sock.connect(this.#integraPort, this.#integraIP, () => res(sock) );
+            sock.once('error', (e) => rej(e) );
+
+            let decoder = new Integra.Decoder();
+            sock.on('data', (data) => {
+                for (let b of data) {
+                    if (decoder.addByte(b)) {
+                        let message = Integra.decodeMessage(decoder);
+                        if (message)
+                            fullFramePF.resolve(message);
+                        else
+                            fullFramePF.reject(new Error("decodeMessage returned null"));
+                    }
+                }
+            });
+        });
+    }
+
+    constructor(integraIP, integraPort) {
+        this.#integraIP = integraIP;
+        this.#integraPort = integraPort;
+    }
+
+    async refreshNames(zones) {
+
+        let fullFramePF = {
+            resolve:  (message) => {},
+            reject: (err) => {}
+        };
+        let sock = await this.#connect(fullFramePF);
+        
+        for (let z = 1; z <= zones.length; ++z) {
+
+            let fullFrameP = new Promise( (res,rej) => { fullFramePF.resolve = res; fullFramePF.reject = rej; });
+            let d = Integra.encodeReadDeviceName(Integra.DeviceType.Zone,z);
+            sock.write(d);
+
+            let message = await fullFrameP;
+            if (message instanceof Integra.ReadDeviceNameAnswer) {
+                assert(z == message.number);
+                zones[message.number-1].name = message.name;
+                process.stdout.write(".");
+            } 
+            else if (message instanceof Integra.CommandResultAnswer) {
+                console.log(`zone=${z} result=${message.resultMessage}`);
+            }
+            else {
+                console.log(`zone=${z} message=${message}`);
+            }
+        }
+        sock.resetAndDestroy();
+        console.log(" DONE");
+
+    }
+
+    async readZoneStates(zones) {
+
+        let fullFramePF = {
+            resolve:  (message) => {},
+            reject: (err) => {}
+        };
+
+        let sock = await this.#connect(fullFramePF);
+        let fullFrameP = new Promise( (res,rej) => { fullFramePF.resolve = res; fullFramePF.reject = rej; });
+        sock.write(Integra.encodeZonesViolationCommand());
+        let message = await fullFrameP;
+        //process.stdout.write("+");
+
+        const flags = message.flags;
+        assert(flags.length >= zones.length);
+
+        let updatedZones = [];
+        for (let i = 0; i < zones.length; ++i) {
+            if (zones[i].state != flags[i]) {
+                zones[i].state = flags[i];
+                updatedZones.push(zones[i]);
+                //console.log(`Zone ${(i+1).toString().padStart(3,'0')}: ${flags[i]}`);
+            }
+        }
+
+        //console.clear();
+        //let dots = flags.reduce( (a,v) =>  a + (v?"●": "·"),"").match(new RegExp(".{1,16}","g"));
+        //for (let l of dots) {
+        //    console.log(l);
+        //}
+
+        sock.resetAndDestroy();
+
+        return updatedZones;
+    }
+}
+
+async function _homeAssistantDiscovery(zones) {
+
     if (!hassDiscoveryTopic)
-        return Promise.resolve;
+        return;
     
     console.log("starting HA discovery");
-    for (let i = 0; i < maxZones; ++i) {
-        let zoneNumStr = (i+1).toString().padStart(3,'0');
+    for (let z of zones) {
+        
+        let zoneNumStr = z.zone_number.toString().padStart(3,'0');
         let zoneId = `integra_zone_${zoneNumStr}`;
+
         await mqttClient.publishAsync(`${hassDiscoveryTopic}/binary_sensor/integra/${zoneId}/config`, 
             JSON.stringify({
-                name: zones[i].name || `Zone ${zoneNumStr}`,
+                name: z.name || `Zone ${zoneNumStr}`,
                 device_class: "motion",
                 state_topic: `${zoneTopic}/${zoneNumStr}`,
                 default_entity_id: `binary_sensor.${zoneId}`,
@@ -114,105 +234,52 @@ async function homeAssistantDiscovery() {
     console.log("HA discovery completed");
 }
 
-async function mainLoop() {
+async function _publishZoneState(zones) {
+    for (let z of zones) {
+        mqttClient.publishAsync(`${zoneTopic}/${(z.zone_number).toString().padStart(3,'0')}`,
+            JSON.stringify({
+                zone_number: z.zone_number,
+                name: z.name,
+                state: z.state? "ON": "OFF"
+            }),
+            {retain: true}
+        );
+    }
+}
+
+
+async function main() {
     
     console.log('Main loop started');
+    let zones = [...Array(maxZones)].map( (e,i) => ({zone_number: i+1, name:'',state:undefined}));
     
-    let interval = setIntervalAsync(2500, Date.now());
-    
-    let zoneViolCmd = Integra.encodeZonesViolationCommand();
     while (true) {
 
-        let frameReceived = new Promise( (r) => fullFrameReceived = r );
+        let cmd = messages.shift();
+                
+        switch (cmd) {
 
-        if (readZoneNames < maxZones) {
-            if (readZoneNames == 0)
-                console.log("Zone names read started")
+            case Messages.RefreshNames:
+                let icRN = new IntegraCommand(integraIP,integraPort);
+                await icRN.refreshNames(zones);
+                continue;
 
-            sock.write(
-                Integra.encodeReadDeviceName(Integra.DeviceType.Zone,++readZoneNames));
+            case Messages.RunHADiscovery:
+                _homeAssistantDiscovery(zones);
+                continue;
 
+            default:
+                await Promise.any([
+                    new Promise( (r) => newMessageArrived = r ),
+                    setTimeoutAsync(1000,0)
+                ]);
 
-            if (readZoneNames < maxZones)
-                process.stdout.write(".");
-            else {
-                console.log(". DONE");
-                homeAssistantDiscovery();
-            }
+            case Messages.ReadZoneStates:
+                let icRZ = new IntegraCommand(integraIP,integraPort);
+                let updatedZones = await icRZ.readZoneStates(zones);
+                await _publishZoneState(updatedZones);
         }
-        else  {
-            sock.write(zoneViolCmd);
-            await interval.next();
-        }
-
-        await frameReceived;
     }
 }
 
-function handleMessage(message) {
-
-    if (message instanceof Integra.ReadDeviceNameAnswer) {
-        zones[message.number-1].name = message.name;
-    }
-    else if (message instanceof Integra.ZonesViolationAnswer) {
-
-        const flags = message.flags;
-        assert(flags.length >= maxZones)
-        for (let i = 0; i < maxZones; ++i) {
-            if (zones[i].state != flags[i]) {
-                zones[i].state = flags[i];
-                //mqttClient.publishAsync(`${zoneTopic}/${(i+1).toString().padStart(3,'0')}`, 
-                //    zones[i].state? "ON": "OFF");
-
-                mqttClient.publishAsync(`${zoneTopic}/${(i+1).toString().padStart(3,'0')}`,
-                    JSON.stringify({
-                        zone_number: i+1,
-                        name: zones[i].name,
-                        state: zones[i].state? "ON": "OFF"
-                    }),
-                    {retain: true}
-                );
-            }
-        }
-
-        //console.clear();
-        //let dots = flags.reduce( (a,v) =>  a + (v?"●": "·"),"").match(new RegExp(".{1,16}","g"));
-        //for (let l of dots) {
-        //    console.log(l);
-        //}
-        
-    }
-}
-
-sock.on('connect', () => {
-    console.log('Connected');
-    mainLoop();
-});
-
-sock.on('close', () => {
-	console.log('connection closed');
-	process.exit(0);
-});
-
-sock.on("error", (e) => { 
-    console.log(`error: ${e}`); 
-    process.exit(1); 
-});
-
-let decoder = new Integra.Decoder();
-sock.on('data', (data) => {
-
-    for (let b of data) {
-        if (decoder.addByte(b)) {
-            fullFrameReceived();
-            let message = Integra.decodeMessage(decoder);
-            if (message)
-                handleMessage(message);
-        }
-    }
-
-});
-
-
-sock.connect(integraPort, integraIP);
-
+main();
---
+++ secrets.js
@@ -0,0 +1,8 @@
+
+const secrets = Object.freeze({
+    integraIP: 'XX.XX.XX.XX',
+    mqttURL: "mqtt://USER:PASSWORD@HOST:PORT",
+});
+
+export default secrets;
+
