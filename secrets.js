4e37af72e615ec12d5df3e291db9a29a7ecd02e2
Sebastian Baberowski <sebastian@baberowski.com>
2025-12-29 17:03:34 +0100

    nsecrets and gitignore

--- secrets.js
+++ secrets.js
@@ -1,7 +1,13 @@
 
+let nonGitSecrets = { default: {} };
+try { nonGitSecrets = await import('./nsecrets.js'); } catch (e) {}
+
 const secrets = Object.freeze({
     integraIP: 'XX.XX.XX.XX',
     mqttURL: "mqtt://USER:PASSWORD@HOST:PORT",
+
+
+    ...nonGitSecrets.default,
 });
 
 export default secrets;
